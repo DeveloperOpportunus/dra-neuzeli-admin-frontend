9859d04917e42a550d11d71d292ccb50fb3c21d4
Rodrigo Lima <developer@opportunusai.email>
2025-11-08 22:14:49 -0300

    function get one pacient

--- backend/src/controllers/pacientes.controller.js
+++ backend/src/controllers/pacientes.controller.js
@@ -15,3 +15,22 @@ export async function listPacientes(req, res) {
         res.status(500).json({ error: e.message });
     }
 }
+
+// GET, retorna paciente por ID
+export async function getPaciente(req, res) {
+    try {
+        const { id } = req.params;
+
+        const { data, error } = await supabaseAdmin
+        .from('pacientes')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+        if (error || !data) return res.status(400).json({ error: 'Paciente não encontrado.' });
+        res.json(data);
+        
+    } catch ( e ) {
+        res.status(500).json({ error: e.message });
+    }
+}
