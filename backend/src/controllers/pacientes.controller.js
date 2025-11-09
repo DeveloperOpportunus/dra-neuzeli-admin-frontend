3c1f903bf8d53b7a9ad01dcb03f42195267f9406
Rodrigo Lima <developer@opportunusai.email>
2025-11-08 22:24:59 -0300

    POST function for registry new pacients

--- backend/src/controllers/pacientes.controller.js
+++ backend/src/controllers/pacientes.controller.js
@@ -29,7 +29,26 @@ export async function getPaciente(req, res) {
 
         if (error || !data) return res.status(400).json({ error: 'Paciente não encontrado.' });
         res.json(data);
+
+    } catch ( e ) {
+        res.status(500).json({ error: e.message });
+    }
+}
+
+// POST, cria um novo paciente.
+export async function createPaciente(req, res) {
+    try {
+        // i'dont know wtf is this but someday i'll figure it out
+        const payload = req.body ?? [];
         
+        const { data, error } = await supabaseAdmin
+        .from('pacientes')
+        .insert(payload)
+        .select()
+        .single();
+
+        if (error) return res.status(400).json({ error: error.message });
+        res.status(201).json(data);
     } catch ( e ) {
         res.status(500).json({ error: e.message });
     }
